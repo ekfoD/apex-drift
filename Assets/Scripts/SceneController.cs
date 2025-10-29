568b477ca86bc225f9514c06fba6dfae546b894d
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2025-10-29 12:27:59 +0200

    Menu UI
    Added menu button logic and Menu UI

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
