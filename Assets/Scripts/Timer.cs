ee5c55a24ee2544af41b080c30101c0b39091c0d
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2025-10-30 00:01:26 +0200

    Setup

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,13 +5,22 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     float elapsedTime;
+    public bool isRunning = true;
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(elapsedTime / 60);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
     }
 }
