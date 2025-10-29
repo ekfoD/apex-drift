fc4e3a06faaba1d9608462bd9d0cbe5e50e79cfe
Dovydas Girskas <dovydas.girskas@unity3d.com>
2025-10-29 21:00:49 +0200

    Added box collider to driving car, added crowd to level 2 and some scripts for animation control

---
+++ Assets/Scripts/AnimationTriggerScriptEnd.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AnimationTriggerEnd : MonoBehaviour
+{
+    public Animator targetAnimator; // Drag your figure here in Inspector
+    public string animationName = "YourAnimationName"; // Or parameter name
+    
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if player/car entered
+        if(other.CompareTag("Player")) // Make sure your player has "Player" tag
+        {
+            // Method 1: Play animation directly
+            targetAnimator.Play(animationName);
+            Debug.Log("ahahaahahah end");
+            
+            // Method 2: Or use trigger parameter
+            // targetAnimator.SetTrigger("StartAnim");
+            
+            // Method 3: Or use bool
+            // targetAnimator.SetBool("isPlaying", true);
+        }
+    }
+}
